9b0ce938fa15f82e3d7dc4d25d8e90d605509836
cdanielson22 <89603429+cdanielson22@users.noreply.github.com>
2023-07-30 22:44:45 -0600

    Add in logic to loop

--- JavaChallenges/If_Else/Solution.java
+++ JavaChallenges/If_Else/Solution.java
@@ -1,12 +1,6 @@
 package JavaChallenges.If_Else;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class Solution {
 
@@ -15,10 +9,35 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int N = 0;
+        String keepGoing = "y";
+        Boolean first = true;
+        while(true){
+            if (first == false){
+                System.out.print("Would you like to continue (y or n): ");
+                keepGoing = scanner.nextLine();
+                first = false;
+            }
+            if (keepGoing == "y"){
+                N = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        scanner.close();
+                if (N % 2 == 1) {
+                    System.out.println("weird");
+                } else {
+                    if (N >= 2 && N <= 5){
+                        System.out.println("Not weird");
+                    } else if (N >= 6 && N <= 20){
+                        System.out.println("weird");
+                    } else {
+                        System.out.println("Not weird");
+                    }
+                }
+            } else {
+                scanner.close();
+            }
+        }
     }
+
 }
 
